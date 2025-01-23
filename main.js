c888f07ba9f00a6f27ba6902217aa06648204ac7
azizbek2510 <azizbekaliyev1011@gmail.com>
2025-01-23 20:33:45 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+const bgColor = prompt("Какой будет цвет фона?");
+let textColor = prompt("Какой будет цвет текста?");
+
+if (bgColor) {
+  document.body.style.backgroundColor = bgColor;
+}
+
+if (textColor) {
+  document.body.style.color = textColor;
+}
+
+
+
+
+
+
+
+
+
